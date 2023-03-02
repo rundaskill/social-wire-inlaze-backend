5a4f20d0dcdae39f053b5e62e50e065691b06ccf
Rundaskill <danielxi@hotmail.es>
2023-03-02 17:26:22 -0500

    docker

--- src/app/mensajes/mensajes.controller.ts
+++ src/app/mensajes/mensajes.controller.ts
@@ -3,8 +3,7 @@ import { MensajesService } from './mensajes.service';
 import { CreateMensajeDto } from './dto/create-mensaje.dto';
 import { UpdateMensajeDto } from './dto/update-mensaje.dto';
 import { JwtAuthGuard } from '../guards/auth/jwt-auth.guard';
-import { FiltroMensajeDto } from './dto/filtro-mensaje.dto';
-import { User } from '../user/entities/user.entity';
+
 
 
 @Controller('mensajes')
@@ -32,23 +31,12 @@ export class MensajesController {
     if (params?.creado_at) {
       return this.mensajesService.findMeMensajeFiltro(req.user,params?.creado_at);
     } 
-   
     return this.mensajesService.findMeMensaje(req.user)
   }
-
- 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.mensajesService.findOne(+id);
-  // }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateMensajeDto: UpdateMensajeDto) {
     return this.mensajesService.update(+id, updateMensajeDto);
   }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.mensajesService.remove(+id);
-  // }
 }
